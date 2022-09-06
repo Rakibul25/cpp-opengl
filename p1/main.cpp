3f6b190f7930501570b2d8cae27da2a18c1682c2
rakib25 <mislam171229@bscse.uiu.ac.bd>
2022-09-06 17:44:51 +0600

    triangel shape

--- p1/main.cpp
+++ p1/main.cpp
@@ -1,3 +1,4 @@
+
 #ifdef __APPLE__
 #include <GLUT/glut.h>
 #else
@@ -5,24 +6,53 @@
 #include<math.h>
 #endif
 
-void display(){
+int dir=0;
+
+void display()
+{
     glClear(GL_COLOR_BUFFER_BIT);
     glLoadIdentity();
+    glPointSize(10.0);
+    glBegin(GL_TRIANGLES);
+        glVertex2d(10, 20);
+        glVertex2d(20, 20);
+        glVertex2d(15, 28.6);
+
+    glEnd();
     glFlush();
+
+
+
+}
+
+void reshape(int h, int w)
+{
+    glViewport(0, 0, w, h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(0, 50, 0, 50);
+    glMatrixMode(GL_MODELVIEW);
+
+}
+
+
+
+void init()
+{
+    glClearColor(0, 0, 0, 0);
 }
 
 int main(int argc, char *argv[])
 {
-    glutInit(&argc,argv);
+    glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_RGB);
-
-    glutInitWindowPosition(200,100);
-    glutInitWindowSize(500,500);
-
-    glutCreateWindow("Hello");
-
+    glutInitWindowPosition(100, 100);
+    glutInitWindowSize(500, 500);
+    glutCreateWindow("CSI422");
+    glutReshapeFunc(reshape);
     glutDisplayFunc(display);
 
+    init();
     glutMainLoop();
 }
 
